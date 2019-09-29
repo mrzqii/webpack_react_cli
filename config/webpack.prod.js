97670f6c9b83fd2f109152ae58fa1c6bdf01056b
zengqi <136800645@qq.com>
2019-09-29 16:03:35 +0800

    add: 在开发模式下面压缩css的代码

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,18 @@
 const webpackConfigCreator = require('./webpack.common');
 const merge = require('webpack-merge');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); //  对css进行压缩
+const safePostCssParser = require('postcss-safe-parser'); // 查找并修复 CSS 语法错误。
 
-const config = {};
+const config = {
+	plugins: [
+		new OptimizeCSSAssetsPlugin({
+			cssProcessorOptions: {
+				parser: safePostCssParser,
+				map: false
+			}
+		})
+	]
+};
 
 const options = {
 	mode: 'production'
